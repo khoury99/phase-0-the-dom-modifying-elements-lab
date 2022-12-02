8ed0978ca9293e440d822b55277fae34c32b7383
Elijah Green <elijah.khoury99@gmail.com>
2022-12-02 12:49:13 -0800

    commit tree e06b3c4aa1ad2901af08c8297ad0f136e41bdb9d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+// const element = document.createElement("div");
+// document.body.append(element);
+// const ul = document.createElement("ul");
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+//   }
+// element.append(ul);
+
+//remove <main> with id 'main'
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Elijah is the champion"
+
